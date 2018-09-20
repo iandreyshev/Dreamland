e6fb05e224a6671d2dcf097f0ca4e0710797bd73
Ivan Andreyshev <ivan.andreyshev@gmail.com>
2018-09-20 07:28:36 +0300

    Создал проект клиента для Android

---
+++ Android/app/src/main/java/ru/iandreyshev/dreamland/presentation/activity/LoginActivity.kt
@@ -0,0 +1,6 @@
+package ru.iandreyshev.dreamland.presentation.activity
+
+import ru.iandreyshev.coreAndroidUtils.activity.BaseAppCompatActivity
+import ru.iandreyshev.dreamland.R
+
+class LoginActivity : BaseAppCompatActivity(R.layout.activity_login)
---
+++ Android/app/src/main/java/ru/iandreyshev/dreamland/presentation/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.iandreyshev.dreamland.presentation.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.intentFor
+import ru.iandreyshev.dreamland.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(intentFor<LoginActivity>())
+    }
+
+}
