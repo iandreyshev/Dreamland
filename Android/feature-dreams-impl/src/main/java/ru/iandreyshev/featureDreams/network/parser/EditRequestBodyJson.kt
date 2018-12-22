d643234a058207c47bf36c5aa61da6d1914a8ff4
Ivan Andreyshev <ivan.andreyshev@ispringsolutions.com>
2018-12-22 13:58:56 +0300

    [Android] Добавил модель JSON для редактирования сна

--- Android/feature-dreams-impl/src/main/java/ru/iandreyshev/featureDreams/network/parser/EditRequestBodyJson.kt
+++ Android/feature-dreams-impl/src/main/java/ru/iandreyshev/featureDreams/network/parser/EditRequestBodyJson.kt
@@ -1,5 +1,15 @@
 package ru.iandreyshev.featureDreams.network.parser
 
+import com.google.gson.annotations.SerializedName
 import ru.iandreyshev.featureDreams.network.request.EditRequest
 
-class EditRequestBodyJson(request: EditRequest)
+class EditRequestBodyJson(request: EditRequest) {
+    @SerializedName("id")
+    val dreamId: Long = request.dreamId
+    @SerializedName("description")
+    val description: String = request.description
+    @SerializedName("is_lucid")
+    val IsLucid: Boolean = request.isLucid
+    @SerializedName("sleeping_date")
+    val sleepingDate: Long = request.sleepingDate
+}
